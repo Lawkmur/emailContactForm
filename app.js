345b6b77741633dd745dbe17c2b3b943f3ef0b08
Fabio <iammasterpvp333@gmail.com>
2021-06-14 07:49:21 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+// View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('contact', {layout:false});
+  app.locals.layout = false;
+});
+
+app.post('/send', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Subject: ${req.body.subject}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Message: ${req.body.message}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  // create reusable transporter object using the default SMTP transport
+ 
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'truckychef@gmail.com',
+      pass: 'Truckychef1_' 
+    }
+  });
+
+  // tls:{
+  //   rejectUnauthorized:false
+  // }
+  // setup email data with unicode symbols
+  
+  let mailOptions = {
+      from: '"Trucky Chef" <truckychef@gmail.com>', // sender address
+      to: 'ricardo.menjivar2022@adoc.superate.org.sv', // list of receivers
+      subject: 'Orden de Comida', // Subject line
+      text: 'Hello world!', // plain text body
+      html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);   
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('contact', {msg:'Email has been sent'});
+  });
+  });
+
+app.listen(3000, () => console.log('Server started...'));
